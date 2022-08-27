522b07403b859c48755946ec82dd8b94c02da6a5
cx33 <lne128gene980@163.com>
2022-08-27 15:28:20 +0800

    chore: clean up & init

--- README.md
+++ README.md
@@ -1,15 +1,9 @@
-# pkg-name
+# cloud-sleep-hub
 
-[![NPM version](https://img.shields.io/npm/v/pkg-name?color=a1b858&label=)](https://www.npmjs.com/package/pkg-name)
+> TODO
 
-## Sponsors
 
-<p align="center">
-  <a href="https://cdn.jsdelivr.net/gh/antfu/static/sponsors.svg">
-    <img src='https://cdn.jsdelivr.net/gh/antfu/static/sponsors.svg'/>
-  </a>
-</p>
 
 ## License
 
-[MIT](./LICENSE) License © 2022 [Anthony Fu](https://github.com/antfu)
+[MIT](./LICENSE) License © 2022 [JiatLn](https://github.com/jiatln)
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,24 @@
-export const one = 1
-export const two = 2
+import { createServer } from 'http'
+import { Server } from 'socket.io'
+
+const httpServer = createServer()
+const io = new Server(httpServer, {
+  cors: {
+    // or with an array of origins
+    origin: [
+      'https://my-frontend.com',
+      'https://my-other-frontend.com',
+      'http://localhost:5577',
+    ],
+    credentials: true,
+  },
+})
+
+io.on('connection', (_socket) => {
+  // ...
+  console.log('a user connected')
+})
+
+httpServer.listen(3000, () => {
+  console.log('listening on *:3000')
+})
