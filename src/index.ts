889c8f1d02d4ac967f75078ff1e3df54c09a102e
cx33 <lne128gene980@163.com>
2022-08-28 17:21:32 +0800

    feat: addUser/userMove listen

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { createServer } from 'http'
 import { Server } from 'socket.io'
+import type { IPeopleInfo } from './types'
 
 const httpServer = createServer()
 const io = new Server(httpServer, {
@@ -14,9 +15,38 @@ const io = new Server(httpServer, {
   },
 })
 
-io.on('connection', (_socket) => {
+io.on('connection', (socket) => {
   // ...
-  console.log('a user connected')
+  const count = io.engine.clientsCount
+  console.log(`current clients count: ${count}`)
+  console.log('a user connected', socket.id)
+  // socket.broadcast.emit('add new user', socket.id)
+
+  socket.on('addUser', (data: Partial<IPeopleInfo>) => {
+    socket.broadcast.emit('[server](addUser)', {
+      ...data,
+      socketId: socket.id,
+    })
+  })
+
+  socket.on('userMove', (data: any) => {
+    socket.broadcast.emit('[server](userMove)', {
+      ...data,
+    })
+  })
+
+  socket.on('disconnect', (reason) => {
+    // ...
+    console.log(reason, socket.id)
+    const count = io.engine.clientsCount
+    console.log(`current clients count: ${count}`)
+    socket.broadcast.emit('[server](userDisconnect)', socket.id)
+  })
+})
+
+io.on('disconnect', (_socket) => {
+  // ...
+  console.log('a user disconnected')
 })
 
 httpServer.listen(3000, () => {
---
+++ src/types.ts
@@ -0,0 +1,10 @@
+export interface Pos {
+  x: number
+  y: number
+}
+
+export interface IPeopleInfo {
+  pos: Pos
+  name: string
+  socketId: string
+}
